ad72dc112068672fab39ae71414588c6b481fec7
karanmaurya15 <karanmaurya9663@gmail.com>
2023-03-29 10:16:43 +0530

    [Karan] Added Signup Page

--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './Components/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { SignupComponent } from './Components/signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
